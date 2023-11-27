3b6f6e3861b79be5b6720f1e9886727d76c985cf
Uaplexer <123483443+Uaplexer@users.noreply.github.com>
2023-11-27 03:17:14 +0200

    Update cities.sql

--- Lab6/cities.sql
+++ Lab6/cities.sql
@@ -10,25 +10,25 @@ WHERE regions.name = 'Nord';
 
 CREATE TABLE Lines
 (
-    id SERIAL PRIMARY KEY,
+    line_id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     color VARCHAR(255)
 );
 
 CREATE TABLE Stations
 (
-    id SERIAL PRIMARY KEY,
+    station_id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     city VARCHAR(255) NOT NULL,
     latitude DOUBLE PRECISION,
     longitude DOUBLE PRECISION,
-    line_id INTEGER REFERENCES Lines(id)
+    line_id INTEGER REFERENCES Lines(line_id)
 );
 
 CREATE TABLE Connections
 (
-    id SERIAL PRIMARY KEY,
-    station_a INTEGER REFERENCES Stations(id),
-    station_b INTEGER REFERENCES Stations(id),
+    connection_id SERIAL PRIMARY KEY,
+    station_a INTEGER REFERENCES Stations(station_id),
+    station_b INTEGER REFERENCES Stations(station_id),
     distance DOUBLE PRECISION
 );
